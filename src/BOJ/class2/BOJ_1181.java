f3acfb14858c18be77ff63cf963581cb3e953eb3
deerKBS <deer980119@gmail.com>
2023-06-03 15:23:56 +0900

    class2 silver5 complete

--- src/BOJ/class2/BOJ_1181.java
+++ src/BOJ/class2/BOJ_1181.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.TreeSet;
 
+// 단어 정렬
 public class BOJ_1181 {
 
 	public static void main(String[] args) throws Exception {
