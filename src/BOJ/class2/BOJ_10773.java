6d866c17e58e2c724108fab969f6a4b8d82eec28
deerKBS <122859252+deerKBS@users.noreply.github.com>
2023-06-07 00:23:52 +0900

    쉬운 Stack 문제, deque를 사용해 풀이

---
+++ src/BOJ/class2/BOJ_10773.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BOJ_10773 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int K = Integer.parseInt(br.readLine());
+		
+		Deque<Integer> d = new ArrayDeque<>();
+		for(int i=0; i<K; i++) {
+			int value = Integer.parseInt(br.readLine());
+			if(value==0) d.pollLast();
+			else d.add(value);
+		}
+		int sum=0;
+		while(!d.isEmpty()) {
+			sum+=d.poll();
+		}
+		System.out.println(sum);
+	}
+}
