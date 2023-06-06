b497661e5227315cdf4b3444084f87b94534e752
deerKBS <122859252+deerKBS@users.noreply.github.com>
2023-06-06 22:34:54 +0900

    BOJ 2751 두 가지 방법이 있는 문제

---
+++ src/BOJ/class2/BOJ_2751.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_2751 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[2000001];
+		for(int i=0; i<N; i++) {
+			int n = Integer.parseInt(br.readLine());
+			arr[n+1000000]=1;
+		}
+		
+		for(int i=0; i<2000001; i++) {
+			if(arr[i]==1) {
+				sb.append(i-1000000).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+}
