2d0483d1165d80a4873847d20d323c84252ecb64
deerKBS <122859252+deerKBS@users.noreply.github.com>
2023-06-06 23:37:33 +0900

    BOJ 4949 간단한 문제. 최적화 할 수 있는 방법이 다양함

---
+++ src/BOJ/class2/BOJ_4949.java
@@ -0,0 +1,46 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ_4949 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			String str = br.readLine();
+			if(str.equals(".")) break;
+			
+			Stack<Character> stack = new Stack<>();
+			String ans = "yes";
+			for(int i=0; i<str.length(); i++) {
+				char c = str.charAt(i);
+				switch(c) {
+				case '(':
+					stack.push('(');
+					break;
+				case ')':
+					if(stack.isEmpty() || stack.pop()!='(') {
+						ans = "no";
+						i=str.length();
+					}
+					break;
+				case '[':
+					stack.push('[');
+					break;
+				case ']':
+					if(stack.isEmpty() || stack.pop()!='[') {
+						ans = "no";
+						i=str.length();
+					}
+					break;
+				}
+			}
+			if(!stack.isEmpty()) ans="no";
+			sb.append(ans).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
