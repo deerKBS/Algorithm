5854a7fd981ff217126b9a0ff6f060cb780f6b0e
deerKBS <deer980119@gmail.com>
2023-06-08 21:49:02 +0900

    easy

--- src/BOJ/class2/BOJ_10816.java
+++ src/BOJ/class2/BOJ_10816.java
@@ -1,9 +1,32 @@
 package BOJ.class2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_10816 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
 		
+		int[] arr = new int[20000001];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			int t = Integer.parseInt(st.nextToken());
+			arr[t+10000000]++;
+		}
+		
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<M; i++) {
+			int t = Integer.parseInt(st.nextToken());
+			sb.append(arr[t+10000000]).append(' ');
+		}
+		
+		System.out.println(sb);
 
 	}
 
