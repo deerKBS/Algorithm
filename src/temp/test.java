4e674449261dbfcec030e963940beba4b37e20d3
deerKBS <deer980119@gmail.com>
2023-06-17 22:50:18 +0900

    현대오토에버 ide 테스트 코드

--- src/temp/test.java
+++ src/temp/test.java
@@ -1,5 +1,14 @@
 package temp;
 
+import java.util.StringTokenizer;
+
 public class test {
-	
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer("aa-bb-cc","-");
+		while(st.hasMoreTokens()) {
+			sb.append(st.nextToken().charAt(0));
+		}
+		System.out.println(sb);
+	}
 }
