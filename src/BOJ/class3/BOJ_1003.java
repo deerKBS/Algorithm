0088901c42cac98cb08dad008c4bec66c383b429
deerKBS <deer980119@gmail.com>
2023-08-22 13:37:56 +0900

    DP refactoring 124ms

--- src/BOJ/class3/BOJ_1003.java
+++ src/BOJ/class3/BOJ_1003.java
@@ -1,30 +1,24 @@
 package BOJ.class3;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class BOJ_1003 {
-    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
 
-        for(int tc=0; tc<T; tc++){
+        for(int t=0; t<T; t++){
             int N = Integer.parseInt(br.readLine());
-            if(N<1) sb.append("1 0\n");
-            else fibonacci(N);
+            int[][] fib = new int[N+3][2];
+            fib[0][0]+=1;
+            fib[1][1]+=1;
+            for(int i=2; i<=N; i++){
+                fib[i][0]=fib[i-2][0]+fib[i-1][0];
+                fib[i][1]=fib[i-2][1]+fib[i-1][1];
+            }
+            sb.append(fib[N][0]).append(' ').append(fib[N][1]).append("\n");
         }
         System.out.print(sb);
-    }
-
-    public static void fibonacci(int N){
-        int[][] fib = new int[N+1][2];
-        fib[0][0]+=1;
-        fib[1][1]+=1;
-        for(int i=2; i<=N; i++){
-            fib[i][0]=fib[i-2][0]+fib[i-1][0];
-            fib[i][1]=fib[i-2][1]+fib[i-1][1];
-        }
-        sb.append(fib[N][0]).append(' ').append(fib[N][1]).append("\n");
     }
 }
