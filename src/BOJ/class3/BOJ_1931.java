99be106f84990823834abbe5b508a96207483483
deerKBS <deer980119@gmail.com>
2023-09-07 09:52:58 +0900

    In progress

---
+++ src/BOJ/class3/BOJ_1931.java
@@ -0,0 +1,13 @@
+package BOJ.class3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_1931 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+
+    }
+}
