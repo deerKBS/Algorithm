6b48d341081307a680d7ef4f54da3b43d5bf5829
deerKBS <deer980119@gmail.com>
2023-09-14 23:09:42 +0900

    색종이 만들기 분할정복, 재귀 196ms

--- src/BOJ/class3/BOJ_2630.java
+++ src/BOJ/class3/BOJ_2630.java
@@ -2,26 +2,23 @@ package BOJ.class3;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.StringTokenizer;
 
 public class BOJ_2630 {
 	
 	static int[][] paper;
 	static int[] count = new int[2];
-	static boolean[][] isChecked;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         paper = new int[N][N];
         for(int i=0; i<N; i++){
-        	String s = br.readLine();
-        	for(int j=0; j<N; j+=2) {
-        		paper[i][j]=s.charAt(j)-'0';
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	for(int j=0; j<N; j++) {
+        		paper[i][j]=Integer.parseInt(st.nextToken());
         	}
         }
-        isChecked = new boolean[N][N];
         
         dfs(0, 0 , N);
         
@@ -29,28 +26,21 @@ public class BOJ_2630 {
     }
     
     static void dfs(int y, int x, int n) {
-    	if(isChecked[y][x]) return;
     	int init = paper[y][x];
     	if(n==1) {
-    		System.out.println(y+", "+x+", "+n);
     		count[init]++;
     		return;
     	}
     	for(int i=y; i<y+n; i++) {
     		for(int j=x; j<x+n; j++) {
-    			if(init==paper[i][j]) continue;
-    			n/=2;
-    			dfs(y,x,n);
-    			dfs(y+n, x, n);
-    			dfs(y, x+n, n);
-    			dfs(y+n, x+n, n);
-    			return;
-    		}
-    	}
-    	System.out.println(y+", "+x+", "+n);
-    	for(int i=y; i<y+n; i++) {
-    		for(int j=x; j<x+n; j++) {
-    			isChecked[i][j]=true;
+    			if(init!=paper[i][j]) {
+    				n/=2;
+        			dfs(y,x,n);
+        			dfs(y+n, x, n);
+        			dfs(y, x+n, n);
+        			dfs(y+n, x+n, n);
+        			return;
+    			}
     		}
     	}
     	count[init]++;
