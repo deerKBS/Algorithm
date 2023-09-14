24ebc36774380c62f8b58ce33cb840ac27cfbfb8
deerKBS <deer980119@gmail.com>
2023-09-14 23:22:53 +0900

    색종이 만들기 분할정복, 재귀 156ms 최적화 완료

--- src/BOJ/class3/BOJ_2630.java
+++ src/BOJ/class3/BOJ_2630.java
@@ -7,42 +7,41 @@ import java.util.StringTokenizer;
 public class BOJ_2630 {
 	
 	static int[][] paper;
-	static int[] count = new int[2];
+	static int blue, white;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
         paper = new int[N][N];
         for(int i=0; i<N; i++){
-        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	st = new StringTokenizer(br.readLine());
         	for(int j=0; j<N; j++) {
-        		paper[i][j]=Integer.parseInt(st.nextToken());
+        		paper[i][j]=st.nextToken().charAt(0)-'0';
         	}
         }
         
         dfs(0, 0 , N);
         
-        System.out.println(count[0]+"\n"+count[1]);
+        System.out.println(white);
+        System.out.println(blue);
     }
     
     static void dfs(int y, int x, int n) {
-    	int init = paper[y][x];
-    	if(n==1) {
-    		count[init]++;
-    		return;
-    	}
+    	int sum=0;
     	for(int i=y; i<y+n; i++) {
     		for(int j=x; j<x+n; j++) {
-    			if(init!=paper[i][j]) {
-    				n/=2;
-        			dfs(y,x,n);
-        			dfs(y+n, x, n);
-        			dfs(y, x+n, n);
-        			dfs(y+n, x+n, n);
-        			return;
-    			}
+    			sum+=paper[i][j];
     		}
     	}
-    	count[init]++;
+    	if(sum==n*n) blue++;
+    	else if(sum==0) white++;
+    	else {
+    		n/=2;
+			dfs(y,x,n);
+			dfs(y, x+n, n);
+			dfs(y+n, x, n);
+			dfs(y+n, x+n, n);
+    	}
     }
 }
