34072d8550416bb248f4763fdc0bb567995de1b0
deerKBS <deer980119@gmail.com>
2023-09-16 23:52:58 +0900

    단지번호붙이기 bfs, (dfs, 그래프) 120ms

--- src/BOJ/class3/BOJ_2667.java
+++ src/BOJ/class3/BOJ_2667.java
@@ -24,10 +24,8 @@ public class BOJ_2667 {
 		List<Integer> ans = new ArrayList<>();
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<N; j++) {
-				if(map[i][j]!='0') {
-					int k = bfs(i,j);
-					ans.add(k);
-					System.out.println(k);
+				if(map[i][j]=='1') {
+					ans.add(bfs(i,j));
 				}
 			}
 		}
@@ -39,7 +37,7 @@ public class BOJ_2667 {
 		System.out.print(sb);
 	}
 	static int bfs(int i, int j) {
-		int count = 1;
+		int count = 0;
 		Queue<int[]> q = new ArrayDeque<>();
 		q.add(new int[] {i, j});
 		map[i][j]='0';
@@ -49,16 +47,14 @@ public class BOJ_2667 {
 			int[] t = q.poll();
 			int y = t[0];
 			int x = t[1];
-			System.out.println(y+", "+x);
 			for(int d=0; d<4; d++) {
 				int ny = y + dy[d];
 				int nx = x + dx[d];
 				if(ny<0 || ny>=N || nx<0 || nx>=N || map[ny][nx]=='0') continue;
 				q.add(new int[] {ny, nx});
-				map[i][j]='0';
+				map[ny][nx]='0';
 			}
 		}
-		
 		return count;
 	}
 }
