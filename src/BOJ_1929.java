f34ffa326a3cb1a975df081b34d7322e40ff628b
deerKBS <deer980119@gmail.com>
2023-09-18 23:45:06 +0900

    소수 구하기

---
+++ src/BOJ_1929.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1929 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] numbers = new int[N + 1];
+        for (int i = 2; i < N + 1; i++) {
+            numbers[i] = i;
+        }
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (numbers[i] == 0) {
+                continue;
+            }
+            for (int j = i + i; j <= N; j += i) {
+                numbers[j] = 0;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = M; i <= N; i++) {
+            if (numbers[i] != 0) {
+                sb.append(numbers[i]).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
